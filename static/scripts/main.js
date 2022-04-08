d388a7db8f0b2a19a12b222fa4cbdbec428a19f1
Negative <50673441+NegativeOfficial@users.noreply.github.com>
2022-04-08 18:40:28 +0300

    Update main.js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,3 @@
 import {generateBombs} from "./game.js"
 let a = Array.from({length: 6}, e => Array.from({length: 12}, e => 0))
-console.log(generateBombs(1, a))
+document.getElementById("dash").innerHTML = generateBombs(1, a).map(x => return x.join(" ") + '\n')
